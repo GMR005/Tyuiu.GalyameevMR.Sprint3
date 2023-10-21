0d972cb967dc93c1755a62701096018b39ed9f54
GalyameevMihail <ruslan28121976@gmail.com>
2023-10-21 18:09:55 +0500

    Task3

--- Tyuiu.GalyameevMR.Sprint3.Task3.V5.Lib/DataService.cs
+++ Tyuiu.GalyameevMR.Sprint3.Task3.V5.Lib/DataService.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;  
 using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.GalyameevMR.Sprint3.Task3.V5.Lib
 {
-    public class DataService : ISprint3Task3V5 
+    public class DataService : ISprint3Task3V5
     {
         public string ReplaceCharInString(string value, char replaceable, char replacement)
         {
