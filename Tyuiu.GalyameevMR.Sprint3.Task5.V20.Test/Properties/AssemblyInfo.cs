accd1e32aa56747f103b4443da8affe906e55ddb
GMR005 <mihailgalemeev@mail.ru>
2023-10-23 17:49:10 +0500

    Task5

---
+++ Tyuiu.GalyameevMR.Sprint3.Task5.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GalyameevMR.Sprint3.Task5.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GalyameevMR.Sprint3.Task5.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("316fa176-dae0-4d93-9a17-93aa6c23c427")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GalyameevMR.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GalyameevMR.Sprint3.Task5.V20.Lib;
+
+namespace Tyuiu.GalyameevMR.Sprint3.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService DataService = new DataService();
+            Console.Title = "Спринт 2. Выполнил: Галямеев М.Р. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("* Спринт #45                                                    ");
+            Console.WriteLine("* Тема: Вложенные циклы                                         ");
+            Console.WriteLine("* Задание #5                                                    ");
+            Console.WriteLine("* Вариант #20                                                   ");
+            Console.WriteLine("* Выполнил: Галямеев М.Р. | ПКТб-23-1                           ");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                      ");
+            Console.WriteLine("На отрезке, где x = 5, вычислить значение функции");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                              *");
+            Console.WriteLine("*X = 5");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                     ");
+            Console.WriteLine("****************************************************************");
+            double res = DataService.GetSumSumSeries(5, 1, 1, 3, 6);
+            Console.WriteLine(res);
+        }
+    }
+}
